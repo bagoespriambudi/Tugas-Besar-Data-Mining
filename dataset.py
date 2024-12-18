a9aee71776deef6a303b0396578dcdfbf1409db8
unknown <faiqalanaking@gmail.com>
2024-12-18 11:20:33 +0700

    Add dashboard functionality

---
+++ dataset.py
@@ -0,0 +1,8 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17) 
+  
+# data (as pandas dataframes) 
+X = breast_cancer_wisconsin_diagnostic.data.features 
+y = breast_cancer_wisconsin_diagnostic.data.targets 
